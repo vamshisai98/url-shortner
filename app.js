be6d436e2ba20f635fca0fe4cabb3a4f97cd7b45
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2020-12-15 22:59:28 +0530

    cors

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const tokenAuth = require('./middlewares/token')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:'*'
+}))
 
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*')
-    next()
-})
+// app.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','*')
+//     next()
+// })
 
 const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017"
 
